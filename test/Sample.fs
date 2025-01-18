bc9a1874a8316e5c82870834a4f9431ae2407949
François-David Collin <Francois-David.Collin@umontpellier.fr>
2025-01-18 03:34:50 +0100

    minor

--- test/Sample.fs
+++ test/Sample.fs
@@ -1,38 +1,72 @@
 module Tests
 
+open System.Text
 open Expecto
+open Yopass
+
+open DotNet.Testcontainers.Builders
+open System
+
+let network = NetworkBuilder().Build()
+
+let memcachedContainer =
+    ContainerBuilder().WithImage("memcached").WithNetwork(network).WithNetworkAliases("memcached").Build()
+
+let yopassContainer =
+    ContainerBuilder()
+        .DependsOn(memcachedContainer)
+        .WithImage("jhaals/yopass")
+        .WithNetwork(network)
+        .WithCommand($"--memcached=memcached:11211")
+        .WithPortBinding("1337", true)
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(fun r -> r.ForPort(uint16 1337)))
+        .Build()
+
+yopassContainer.StartAsync().Wait()
+
+let yopassUrl =
+    UriBuilder("http", yopassContainer.Hostname, int (yopassContainer.GetMappedPublicPort(1337)))
+        .Uri.ToString()
+        .TrimEnd('/')
+
+let setupYopass f () =
+    let yopass = Wrapper(yopassUrl, yopassUrl)
+    f yopass
+
+let fixtureTests =
+    testFixture setupYopass
+    <| [ "encrypt a simple string with a generated key and decrypt it",
+         fun yopass ->
+             let someKey = Crypt.generateKey ()
+             yopass.Key <- Some someKey
+             let input = "hello"
+             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+             let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+
+             Expect.equal input decrypted "The decrypted string should be the same as the input"
+         "encrypt a simple string with a generated key and try do decrypt with the wrong key",
+         fun yopass ->
+             yopass.Key <- Some(Crypt.generateKey ())
+             let input = "hello"
+             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+             yopass.Key <- Some(Crypt.generateKey ())
+
+             let tryDecrypt =
+                 try
+                     encrypted |> yopass.decrypt |> ignore
+                     false
+                 with _ ->
+                     true
+
+             Expect.isTrue tryDecrypt "This should fail because the key is wrong"
+         "encrypt a simple string, no password and decrypt it (use the provided key)",
+         fun yopass ->
+             let input = "hello"
+             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+             let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+
+             Expect.equal input decrypted "The decrypted string should be the same as the input" ]
+    |> List.ofSeq
 
 [<Tests>]
-let tests =
-
-  
-  testList "samples" [
-    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
-
-    testCase "when true is not (should fail)" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false"
-
-    testCase "I'm skipped (should skip)" <| fun _ ->
-      Tests.skiptest "Yup, waiting for a sunny day..."
-
-    testCase "I'm always fail (should fail)" <| fun _ ->
-      Tests.failtest "This was expected..."
-
-    testCase "contains things" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-                         "This is the case; {2,3,4} contains {2,4}"
-
-    testCase "contains things (should fail)" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4; 1 |]
-                         "Expecting we have one (1) in there"
-
-    testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
-      Expect.equal "abcdëf" "abcdef" "These should equal"
-
-    test "I am (should fail)" {
-      "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true false
-    }
-  ]
+let tests = testList "samples" fixtureTests |> testSequenced
