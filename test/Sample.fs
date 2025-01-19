abce8372a9a7531c49f60a036af2afbaa7eafd97
François-David Collin <Francois-David.Collin@umontpellier.fr>
2025-01-19 05:19:01 +0100

    test test

--- test/Sample.fs
+++ test/Sample.fs
@@ -5,6 +5,7 @@ open Expecto
 open Yopass
 
 open DotNet.Testcontainers.Builders
+open DotNet.Testcontainers.Containers
 open System
 
 let network = NetworkBuilder().Build()
@@ -12,61 +13,102 @@ let network = NetworkBuilder().Build()
 let memcachedContainer =
     ContainerBuilder().WithImage("memcached").WithNetwork(network).WithNetworkAliases("memcached").Build()
 
-let yopassContainer =
+let yopassContainer args =
+    let cmdargs = [ "--memcached=memcached:11211" ] @ args |> Array.ofList
+
     ContainerBuilder()
         .DependsOn(memcachedContainer)
         .WithImage("jhaals/yopass")
         .WithNetwork(network)
-        .WithCommand($"--memcached=memcached:11211")
+        .WithCommand(cmdargs)
         .WithPortBinding("1337", true)
         .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(fun r -> r.ForPort(uint16 1337)))
+        // .WithOutputConsumer(Consume.RedirectStdoutAndStderrToConsole())
         .Build()
 
-yopassContainer.StartAsync().Wait()
+let yopassWrapper (container: IContainer) =
+    let yopassURL =
+        UriBuilder("http", container.Hostname, int (container.GetMappedPublicPort(1337))).Uri.ToString().TrimEnd('/')
 
-let yopassUrl =
-    UriBuilder("http", yopassContainer.Hostname, int (yopassContainer.GetMappedPublicPort(1337)))
-        .Uri.ToString()
-        .TrimEnd('/')
+    Wrapper(yopassURL, yopassURL)
 
-let setupYopass f () =
-    let yopass = Wrapper(yopassUrl, yopassUrl)
-    f yopass
+type SetupYopassContainer(args) =
+    let container = yopassContainer args
+    do container.StartAsync().Wait()
 
-let fixtureTests =
-    testFixture setupYopass
-    <| [ "encrypt a simple string with a generated key and decrypt it",
-         fun yopass ->
-             let someKey = Crypt.generateKey ()
-             yopass.Key <- Some someKey
-             let input = "hello"
-             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
-             let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+    member x.setupYopass f () =
+        let yopass = yopassWrapper container
+        f yopass
 
-             Expect.equal input decrypted "The decrypted string should be the same as the input"
-         "encrypt a simple string with a generated key and try do decrypt with the wrong key",
-         fun yopass ->
-             yopass.Key <- Some(Crypt.generateKey ())
-             let input = "hello"
-             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
-             yopass.Key <- Some(Crypt.generateKey ())
-
-             let tryDecrypt =
-                 try
-                     encrypted |> yopass.decrypt |> ignore
-                     false
-                 with _ ->
-                     true
-
-             Expect.isTrue tryDecrypt "This should fail because the key is wrong"
-         "encrypt a simple string, no password and decrypt it (use the provided key)",
-         fun yopass ->
-             let input = "hello"
-             let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
-             let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
-
-             Expect.equal input decrypted "The decrypted string should be the same as the input" ]
-    |> List.ofSeq
+let setupYopass_normal = SetupYopassContainer([]).setupYopass
+let setupYopass_long = SetupYopassContainer([ "--max-length=1000000" ]).setupYopass
 
 [<Tests>]
-let tests = testList "samples" fixtureTests |> testSequenced
+let fixtureTests =
+    testFixture
+        setupYopass_normal
+        [ "encrypt a simple string with a generated key and decrypt it",
+          fun yopass ->
+              let someKey = Crypt.generateKey ()
+              yopass.Key <- Some someKey
+              let input = "hello"
+              let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+              let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+
+              Expect.equal input decrypted "The decrypted string should be the same as the input"
+          "encrypt a simple string with a generated key and try do decrypt with the wrong key",
+          fun yopass ->
+              yopass.Key <- Some(Crypt.generateKey ())
+              let input = "hello"
+              let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+              yopass.Key <- Some(Crypt.generateKey ())
+
+              let tryDecrypt =
+                  try
+                      encrypted |> yopass.decrypt |> ignore
+                      false
+                  with _ ->
+                      true
+
+              Expect.isTrue tryDecrypt "This should fail because the key is wrong"
+          "encrypt a simple string, no password and decrypt it (use the provided key)",
+          fun yopass ->
+              let input = "hello"
+              let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+              let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+
+              Expect.equal input decrypted "The decrypted string should be the same as the input"
+          "set onetime, encrypt a simple string, no password, decrypt it once and fail to decryt again",
+          fun yopass ->
+              yopass.OneTime <- true
+              let input = "hello"
+              let encrypted = input |> Encoding.UTF8.GetBytes |> yopass.encrypt
+              let decrypted = encrypted |> yopass.decrypt |> Encoding.UTF8.GetString
+
+              Expect.equal input decrypted "The decrypted string should be the same as the input"
+
+              let tryDecrypt =
+                  try
+                      encrypted |> yopass.decrypt |> ignore
+                      false
+                  with _ ->
+                      true
+
+              Expect.isTrue tryDecrypt "This should fail because the secret is gone" ]
+    |> Seq.toList
+    |> testList "samples normal"
+
+[<Tests>]
+let fixtureLongTests =
+    testFixture
+        setupYopass_long
+        [ "encrypt a (long) binary random data and decrypt it",
+          fun yopass ->
+              let input = Array.randomChoices 100000 [| 0uy .. 255uy |]
+
+              let encrypted = input |> yopass.encrypt
+              let decrypted = encrypted |> yopass.decrypt
+
+              Expect.equal input decrypted "The decrypted string should be the same as the input" ]
+    |> Seq.toList
+    |> testList "samples long"
